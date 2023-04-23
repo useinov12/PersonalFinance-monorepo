8132f1424d99e07a6d6135dc53c084e4cc33f4b3
Ruslan Useinov <r.useinov23@gmail.com>
2023-04-23 19:06:14 -0400

    third party url config for next/Image

--- next.config.mjs
+++ next.config.mjs
@@ -20,5 +20,15 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+          protocol: 'https',
+          hostname: '**',
+          port: '',
+          pathname: '**',
+      }
+    ],
+  },
 };
 export default config;
